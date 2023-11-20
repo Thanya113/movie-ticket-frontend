75b907d185a8e0fb817599fae567276f4c617672
Thanya113 <thanyarg.21it@kongu.edu>
2023-11-20 12:14:49 +0530

    initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import {Box, Typography,Button} from "@mui/material";
+import MovieItem from './Movies/MovieItem';
+import {Link} from "react-router-dom";
+import { getAllMovies } from '../api-helpers/api-helpers';
+const HomePage = () => {
+    const [movies, setMovies]=useState([]);
+    useEffect(() => {
+        getAllMovies()
+          .then((data) => {
+            console.log("Movie data:", data);
+            setMovies(data.movies);
+          })
+          .catch((err) => console.error("Error fetching movies:", err));
+      }, []);
+      
+
+
+  return <Box width={"100%"} height="100%" margin="auto" marginTop={2} >
+  <Box margin={"auto"} width="80%" height={"40vh"} padding={2}>
+    <img src="https://i.ytimg.com/vi/8K7KqCKqlKs/maxresdefault.jpg" alt="Leo" width={"100%"} height={"100%"} />
+  </Box>
+  <Box padding={5} margin="auto">
+    <Typography variant="h4" textAlign={"center"}  >Latest Releases</Typography>
+  </Box>
+  <Box margin={"auto"} display="flex" width="80%" justifyContent={"center"} alignItems="center" flexWrap="wrap" gap={2} >
+
+    {movies && movies.slice(0,4).map((movie,index)=> (<MovieItem id={movie.id} title={movie.title} posterUrl={movie.posterUrl} releaseDate={movie.releaseDate} key={index} />))}
+  </Box>
+  <Box display="flex" padding={5} margin="auto">
+    <Button LinkComponent={Link} to="/movies" variant="outlined" sx={{margin:'auto', color:"#2b2d42"}}>
+        View all Movies
+    </Button>
+  </Box>
+    </Box>;
+  
+};
+
+export default HomePage
